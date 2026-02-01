756d8dcb8cfb951e8d153e1fad35f3a88d63fe0b
retore404 <44674058+retore404@users.noreply.github.com>
2026-02-01 10:14:30 +0900

    drawer追加

--- app/app/welcome/welcome.tsx
+++ app/app/welcome/welcome.tsx
@@ -1,10 +1,10 @@
+import { DrawerScrollableContent } from "~/components/content/DrawerScrollableContent";
 
 
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <span>hogehoge</span>
+      <DrawerScrollableContent />
     </main>
   );
 }
-
