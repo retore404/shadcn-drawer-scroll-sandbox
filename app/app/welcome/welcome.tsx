8ccadb488d8584d474aca064a3ccb6e44ff3b1c3
retore404 <44674058+retore404@users.noreply.github.com>
2026-02-02 18:50:28 +0900

    mainスタイル見直し

--- app/app/welcome/welcome.tsx
+++ app/app/welcome/welcome.tsx
@@ -6,7 +6,7 @@ import { Button } from "~/components/ui/button";
 export function Welcome() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main className="flex items-center justify-center">
       <section>
         <Button onClick={() => setIsDrawerOpen(true)}>Open Welcome Drawer</Button>
       </section>
